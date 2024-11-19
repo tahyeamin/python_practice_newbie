ac8fb3bbd1f73e8005efc81dd7ec9319ed3a85ff
tahyeamin <83492183+tahyeamin@users.noreply.github.com>
2024-11-19 21:24:29 +0600

    u

--- dictionary.py
+++ dictionary.py
@@ -2,9 +2,28 @@ dictionary= {
 
 "brand": "ford",
 "model ": "Mustand",
+"model " : "Expedition",  #Duplicate values will overwrite existing values
 "year": 1976
 
 
 }
 
 print(dictionary)
+
+print(dictionary["brand"])  #accessing value without using get function
+
+print(dictionary["model "]) #string should be same, Accessing value using key 
+
+
+
+print(dictionary.get("year")) #accessing value using get function
+
+print(len(dictionary))
+
+print(type(dictionary))
+
+k = dictionary.keys()
+
+print(k)
+
+
--- tuple.py
+++ tuple.py
@@ -87,3 +87,5 @@ print(count)
 
 
 
+
+
